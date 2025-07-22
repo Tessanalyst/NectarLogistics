9eabee4ae1cf3090e00f766d94ac7ec924858825
CHIMUANYA THERESA ANEGBE <151169323+Tessanalyst@users.noreply.github.com>
2025-07-22 10:31:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.', // Current directory
-  build: {
-    outDir: 'dist/client'
-  }
+  plugins: [react()],
 })
